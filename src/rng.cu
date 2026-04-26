a7b85d8716c9949f24c52353dd935db684f94e34
Aaron Li <107497659+AAce3@users.noreply.github.com>
2026-04-26 11:18:30 -0400

    add skeleton

--- src/rng.cu
+++ src/rng.cu
@@ -1,3 +1,5 @@
+#include "../include/sim.cuh"
+
 // Initial neutron energies
 // Initial angular position
 // Initial radial position
@@ -6,14 +8,22 @@
 // Collision type selector
 // Number of fission neutrons produced (2 or 3)
 
-// #include "common.h"
-#include "../include/sim.cuh"
+__device__ float random_uniform(unsigned int *state)
+{
+    // TODO: Replace with cuRAND or a higher-quality RNG if needed.
+}
 
-struct init_neutron_energy {
-   // Use Maxwell Boltzman distribution
+__device__ void sample_isotropic_direction(unsigned int *state, float *ux, float *uy)
+{
+    // TODO: Sample an isotropic 2D direction using random_uniform().
+}
+
+__global__ void init_rng_kernel()
+{
+    // TODO: Initialize one RNG state per CUDA thread/history.
 }
 
 void rng_wrapper()
 {
-
+    // TODO: Allocate, initialize, and manage RNG state buffers from host code.
 }
